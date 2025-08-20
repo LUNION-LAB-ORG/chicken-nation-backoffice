ee54a7323d2ccd0ef0d64cc2ec87a14a7bb7ee89
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-08-20 16:38:54 +0000

    mise à jour de l'impression

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -1,13 +1,45 @@
 import React from "react";
 import { Content } from "./Content";
+import { cookies } from "next/headers";
+import { OrderData } from "./types";
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+async function getOrder(token: string, id: string): Promise<OrderData> {
+  const baseURL =
+    process.env.NEXT_PUBLIC_API_PREFIX || "http://localhost:8081/api/v1";
+
+  const response = await fetch(`${baseURL}/orders/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    return null;
+  }
+
+  return response.json();
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
-
   if (!id) {
     return <div>Erreur: ID de commande non fourni</div>;
   }
-  return <Content id={id} />;
-};
+  const cookieStore = await cookies();
+  const token = cookieStore.get("chicken-nation-token");
 
-export default Page;
+  if (!token) {
+    return <div>Erreur: Utilisateur non authentifié</div>;
+  }
+  const data = await getOrder(token.value, id);
+  if (!data) {
+    return <div>Erreur: Commande non trouvée</div>;
+  }
+  // Requête pour récupérer les données de la commande
+  return <Content orderData={data} />;
+}
