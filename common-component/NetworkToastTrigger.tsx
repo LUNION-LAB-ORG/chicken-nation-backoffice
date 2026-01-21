046c1457822912ae53565588067e530c0483328d
Anderson Kouadio <andersonkouadio0118@gmail.com>
2026-01-21 17:10:08 +0000

    mise à jour des permissions medias

--- common-component/NetworkToastTrigger.tsx
+++ common-component/NetworkToastTrigger.tsx
@@ -1,22 +1,18 @@
-'use client';
+"use client";
 
-import { useNetworkStatus } from "@/hooks/useOfflineBanner copy";
+import { useNetworkStatus } from "@/hooks/useNetworkStatus";
 import { toast } from "react-hot-toast";
-
-
-
-
 
 export function NetworkToastTrigger() {
   const isOnline = useNetworkStatus();
 
   return (
-    <div
-
-    >
+    <div>
       {isOnline
-        ? toast.success('✅ Vous êtes en ligne')
-        : toast.error('⚠️ Vous êtes hors ligne. Certaines fonctionnalités peuvent ne pas fonctionner.')}
+        ? toast.success("✅ Vous êtes en ligne")
+        : toast.error(
+            "⚠️ Vous êtes hors ligne. Certaines fonctionnalités peuvent ne pas fonctionner.",
+          )}
     </div>
   );
 }
